00ffa5e3ccfbb376fca8731ac07cbef59b41a7ac
jairus.bowne <jairus.bowne@0274baff-0e29-717f-932a-951b5de94a22>
2010-09-16 05:11:10 +0000

    Added external vector normalisation. Updated docs

--- R/heatmap.r
+++ R/heatmap.r
@@ -55,3 +55,127 @@ pic_onscr<-function(matrix, title="", cex_val=1, pal=rch)
     }
 # Draw the heatmap
 pic_onscr(t(heatmap_data))
+
+#
+#    Generate figure as image file
+#
+#    (Uncomment blocks as necessary)
+
+# ##### jpg #####
+# pic_jpg<-function(filename, matrix, title="", cex_val=1, pal=rch)
+#     {# Start jpg device with basic settings
+#     ## Fiddle with the width and height settings to get the key to
+#     ## display with a 3:2 aspect ratio for best results. 8.3x5.8 is A5.
+#     jpeg(filename,
+#         quality=100,                       # image quality (percent)
+#         bg="white",                        # background colour
+#         res=300,                           # image resolution (dpi)
+#         units="in", width=8.3, height=5.8) # image dimensions (inches)
+#         par(mgp=c(3,1,0),                  # axis margins
+#                                            # (title, labels, line)
+#             mar=c(5,4,4,2)                 # plot margins (b,l,t,r)
+#             )
+#     # Draw the plot
+#      heatmap.2(matrix,                     # matrix to use
+#         #Colv=FALSE,                       # reorder dendrogram
+#         dendrogram="both",                 # dendrograms to draw
+#         col=pal,                           # palette (see above)
+#         trace="none",                      # trace line (show distance)
+#         cexCol=cex_val,                    # size of column labels
+#         cexRow=cex_val,                    # size of row labels
+#         key=TRUE,                          # show the colour key
+#         keysize=cex_val,                   # key size
+#         density.info="none",               # superimpose "histogram" or a
+#                                            # "density" plot on colour key
+#         margins=c(5,5),                    # add space for labels
+#         main=title,                        # title
+#         xlab="",                           # x-axis title
+#         ylab=""                            # y-axis title
+#         )
+#     # Turn off the device
+#     dev.off()
+#     }
+# pic_jpg("heatmap.jpg", t(heatmap_data))
+# # For larger text, change the cex_val:
+# # pic_jpg("heatmap.jpg", t(heatmap_data), cex_val=3)
+# ##### end jpg #####
+
+
+#### png #####
+# pic_png<-function(filename, matrix, title="", cex_val=1, pal=rch)
+#     {# Start png device with basic settings
+#     ## Fiddle with the width and height settings to get the key to
+#     ## display with a 3:2 aspect ratio for best results. 8.3x5.8 is A5.
+#     png(filename,
+#         bg="white",                        # background colour
+#         res=300,                           # image resolution (dpi)
+#         units="in", width=5.8, height=6.5) # image dimensions (inches)
+#         par(mgp=c(3,1,0),                  # axis margins
+#                                            # (title, labels, line)
+#             mar=c(5,4,4,2)                 # plot margins (b,l,t,r)
+#             )
+#     # Draw the plot
+#      heatmap.2(matrix,                     # matrix to use
+#         #Colv=FALSE,                       # reorder dendrogram
+#         dendrogram="both",                 # dendrograms to draw
+#         col=pal,                           # palette (see above)
+#         trace="none",                      # trace line (show distance)
+#         cexCol=cex_val,                    # size of column labels
+#         cexRow=cex_val,                    # size of row labels
+#         key=TRUE,                          # show the colour key
+#         keysize=cex_val,                   # key size
+#         density.info="none",               # superimpose "histogram" or a
+#                                            # "density" plot on colour key
+#         margins=c(10,10),                  # add space for labels
+#         main=title,                        # title
+#         xlab="",                           # x-axis title
+#         ylab=""                            # y-axis title
+#         )
+#     # Turn off the device
+#     dev.off()
+#     }
+# pic_png("heatmap.png", t(heatmap_data))
+# # For larger text, change the cex_val:
+# # pic_png("heatmap.png", t(heatmap_data), cex_val=3)
+#### end png #####
+
+
+# ##### tiff #####
+# pic_tiff<-function(filename, matrix, title="", cex_val=1, pal=rch)
+#     {# Start tiff device with basic settings
+#     ## Fiddle with the width and height settings to get the key to
+#     ## display with a 3:2 aspect ratio for best results. 8.3x5.8 is A5.
+#     tiff(filename,
+#         bg="white",                        # background colour
+#         res=300,                           # image resolution (dpi)
+#         units="in", width=8.3, height=5.8) # image dimensions (inches)
+#         compression="none"                 # image compression 
+#                                            #  (one of none, lzw, zip)
+#         par(mgp=c(3,1,0),                  # axis margins
+#                                            # (title, labels, line)
+#             mar=c(5,4,4,2)                 # plot margins (b,l,t,r)
+#             )
+#     # Draw the plot
+#      heatmap.2(matrix,                     # matrix to use
+#         #Colv=FALSE,                       # reorder dendrogram
+#         dendrogram="both",                 # dendrograms to draw
+#         col=pal,                           # palette (see above)
+#         trace="none",                      # trace line (show distance)
+#         cexCol=cex_val,                    # size of column labels
+#         cexRow=cex_val,                    # size of row labels
+#         key=TRUE,                          # show the colour key
+#         keysize=cex_val,                   # key size
+#         density.info="none",               # superimpose "histogram" or a
+#                                            # "density" plot on colour key
+#         margins=c(5,5),                    # add space for labels
+#         main=title,                        # title
+#         xlab="",                           # x-axis title
+#         ylab=""                            # y-axis title
+#         )
+#     # Turn off the device
+#     dev.off()
+#     }
+# pic_tiff("heatmap.tif", t(heatmap_data))
+# # For larger text, change the cex_val:
+# # pic_tiff("heatmap.tif", t(heatmap_data), cex_val=3)
+# ##### end tiff #####
---
+++ R/norm_ext_vec.r
@@ -0,0 +1,53 @@
+#    norm_IS.r
+#
+#    Author:    Amsha Nahid, Jairus Bowne, Gerard Murray
+#    Purpose:    Normalise a data matrix using an internal standard
+#
+#    Inputs:    Data matrix as specified in Data-matrix-format.pdf
+#               Another matrix containing the normalisation factor
+#    Output:    Normalised data matrix (.csv format)
+#
+#    Notes:    Normalisation vector must be of similar form to data
+#              matrix - that is, rows are samples, variable is one
+#              column with values for normalising against. Length must
+#              match data matrix length
+#
+#    Load and prepare the data matrix
+#
+# Read in the .csv file
+data<-read.csv("input.csv", sep=",", row.names=1, header=TRUE)
+# Get normalisation vector
+norm_vector<-read.csv("norm_vector.csv",sep=",",row.names=1,header=TRUE)
+# Get groups information
+Group<-data[,1]
+# Remove groups for data processing
+pre_norm<-data[,-1]
+
+# Prepare an empty matrix
+norm_data<-matrix(NA,nrow=nrow(pre_norm),ncol=length(pre_norm))
+rownames(norm_data)<-rownames(pre_norm)
+colnames(norm_data)<-colnames(pre_norm)
+
+#
+#    Normalise the data
+#
+for (ii in 1:nrow(pre_norm)) {
+    for (jj in 1:length(pre_norm)) {   # this gives the row length
+        norm_data[ii,jj]<-pre_norm[ii,jj]/norm_vector[ii]
+        }
+    }
+# Reattach groups information
+output<-cbind(data.frame(Group),norm_data)
+
+# Edit the column names if necessary
+colnames(output) <- if
+    (length(grep("^X[\\d]",colnames(output),perl=TRUE)) != 0) # then
+    {gsub("^X([\\d].*)","\\1",colnames(output),perl=TRUE)} else
+    {colnames(output)}
+
+#
+# Generate the output matrix in .csv format
+#
+write.csv(output,"norm_data_EV.csv")
+
+
